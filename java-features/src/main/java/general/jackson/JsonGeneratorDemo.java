4abb99961a4d2c401e2e8b58e723e1ac77c09193
Gor Petrosyan <gpetrosay.ggm@gmail.com>
2022-01-19 18:34:05 +0400

    fixed jackson generator demo

--- java-features/src/main/java/general/jackson/JsonGeneratorDemo.java
+++ java-features/src/main/java/general/jackson/JsonGeneratorDemo.java
@@ -1,9 +1,11 @@
 package general.jackson;
 
 import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 public class JsonGeneratorDemo {
 
@@ -13,24 +15,26 @@ public class JsonGeneratorDemo {
         simpleDemo(factory);
     }
 
-    // TODO fix
     private static void simpleDemo(JsonFactory factory) throws IOException {
-        JsonGenerator generator = factory.createGenerator(
-                new File("generator-output.json"),
-                JsonEncoding.UTF8);
+        final String filePath = "java-features/src/main/resources/generator-output.json";
+        JsonGenerator generator = factory.createGenerator(new File(filePath), JsonEncoding.UTF8);
 
         generator.writeStartObject();
         generator.writeStringField("brand", "Mercedes");
         generator.writeNumberField("doors", 5);
-        generator.writeStartArray("specs");
+        generator.writeFieldName("specs");
         generator.writeStartArray();
-        generator.writeString("aaa");
-        generator.writeString("bbb");
-        generator.writeString("ccc");
+        generator.writeNumber(11);
+        generator.writeNumber(22);
+        generator.writeNumber(33);
         generator.writeEndArray();
         generator.writeEndObject();
 
         generator.close();
+
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> dataMap = mapper.readValue(new File(filePath), Map.class);
+        System.out.println(dataMap);
     }
 
 }
